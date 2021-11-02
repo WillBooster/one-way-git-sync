31fda395b235dd0dcd40116e968c7d0c70b90e7a
Sakamoto, Kazunori <exKAZUu@users.noreply.github.com>
2021-11-02 17:51:44 +0900

    feat!: allow users not to create a tag (#32)

--- src/index.ts
+++ src/index.ts
@@ -5,53 +5,52 @@ import path from 'path';
 import fse from 'fs-extra';
 import simpleGit, { SimpleGit } from 'simple-git';
 import type { LogResult } from 'simple-git/typings/response';
+import { InferredOptionTypes } from 'yargs';
 import { hideBin } from 'yargs/helpers';
 import yargs from 'yargs/yargs';
 
 const SYNC_DIR_PATH = '../sync-git-repo';
 const ignoreNames = ['.git', 'node_modules'];
 
-interface Settings {
-  dest: string;
-  prefix?: string;
-  dry?: boolean;
-  force?: boolean;
-}
+const builder = {
+  dest: {
+    type: 'string',
+    alias: 'd',
+    describe: 'A URL of a destination git repository',
+    demand: true,
+  },
+  prefix: {
+    type: 'string',
+    alias: 'p',
+    describe: `A prefix of a commit hash used to generate a commit message.
+                 The typical value is like "https://github.com/WillBooster/one-way-git-sync/commits/"`,
+  },
+  tag: {
+    type: 'boolean',
+    alias: 't',
+    describe: 'Create version+hash tag (e.g. v1.31.5-2-gcdde507). This should be a unique tag.',
+  },
+  'tag-version': {
+    type: 'boolean',
+    alias: 'tv',
+    describe: 'Create version tag (e.g. v1.31.5). This may be a non-unique tag.',
+  },
+  dry: {
+    type: 'boolean',
+    describe: 'Enable dry-run mode',
+  },
+  force: {
+    type: 'boolean',
+    describe: 'Force to overwrite the destination git repository',
+  },
+} as const;
 
 export async function cli(argv: string[]): Promise<void> {
-  const parsed = await yargs(hideBin(argv)).options({
-    dest: {
-      type: 'string',
-      alias: 'd',
-      describe: 'A URL of a destination git repository',
-      demand: true,
-    },
-    prefix: {
-      type: 'string',
-      alias: 'p',
-      describe: `A prefix of a commit hash used to generate a commit message.
-                 The typical value is like "https://github.com/WillBooster/one-way-git-sync/commits/"`,
-    },
-    dry: {
-      type: 'boolean',
-      describe: 'Enable dry-run mode',
-    },
-    force: {
-      type: 'boolean',
-      describe: 'Force to overwrite the destination git repository',
-    },
-  }).argv;
+  const parsed = await yargs(hideBin(argv)).options(builder).argv;
   await main(parsed);
 }
 
-async function main(settings: Settings): Promise<void> {
-  // e.g. `--abbrev=0` changes `v1.31.5-2-gcdde507` to `v1.31.5`
-  const version = child_process.execSync('git describe --tags --always --abbrev=0').toString().trim();
-  if (!/^v\d/.test(version)) {
-    console.error('Please run on the `vX.X.X` tag.');
-    process.exit(1);
-  }
-
+async function main(settings: InferredOptionTypes<typeof builder>): Promise<void> {
   const srcGit: SimpleGit = simpleGit();
 
   await fsp.rm(SYNC_DIR_PATH, { recursive: true, force: true });
@@ -93,6 +92,9 @@ async function main(settings: Settings): Promise<void> {
   }
   await dstGit.add('-A');
 
+  // e.g. `--abbrev=0` changes `v1.31.5-2-gcdde507` to `v1.31.5`
+  const describeCommand = `git describe --tags --always ${settings['tag-version'] ? '--abbrev=0' : ''}`;
+  const version = child_process.execSync(describeCommand).toString().trim();
   const title = `sync ${version} (${settings.prefix || ''}${latestHash})`;
   const body = srcLog.all.map((l) => `* ${l.message}`).join('\n\n');
   try {
@@ -104,12 +106,15 @@ async function main(settings: Settings): Promise<void> {
     process.exit(1);
   }
 
-  try {
-    await dstGit.addTag(version);
-    console.log(`Created a tag: ${version}`);
-  } catch (e) {
-    console.error('Failed to commit changes:', e);
-    process.exit(1);
+  const shouldCreateTag = settings.tag || settings['tag-version'];
+  if (shouldCreateTag) {
+    try {
+      await dstGit.addTag(version);
+      console.log(`Created a tag: ${version}`);
+    } catch (e) {
+      console.error('Failed to commit changes:', e);
+      process.exit(1);
+    }
   }
 
   if (settings.dry) {
@@ -119,7 +124,9 @@ async function main(settings: Settings): Promise<void> {
 
   try {
     await dstGit.push();
-    await dstGit.push({ '--tags': null });
+    if (shouldCreateTag) {
+      await dstGit.push({ '--tags': null });
+    }
   } catch (e) {
     console.error('Failed to push a commit:', e);
     process.exit(1);
